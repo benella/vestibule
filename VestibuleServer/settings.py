f8533f956f299b46cc30c5fa6f2eea095dbd6228
Ben <BenElla91@gmail.com>
2020-04-04 17:22:50 +0300

    adding raspberry pi IP

--- VestibuleServer/settings.py
+++ VestibuleServer/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0',
                  '127.0.0.1',
-                 '192.168.0.15']
+                 '192.168.0.15',
+                 '192.168.0.17']
 
 
 # Application definition
