2d5bdaa011ef5a1b78cd46652b6427b51694ca67
Ben <BenElla91@gmail.com>
2020-05-01 15:45:21 +0300

    changed show info update interval to 12h

--- shows/show_info_update/routine.py
+++ shows/show_info_update/routine.py
@@ -4,7 +4,7 @@ from .shows_updater import update_shows
 
 
 def start():
-    minutes_interval = 1440
+    minutes_interval = 720
     scheduler = BackgroundScheduler()
     scheduler.start()
     scheduler.add_job(update_shows, 'interval', minutes=minutes_interval)
