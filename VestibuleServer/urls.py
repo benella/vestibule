31a8ecab75c207634ff2ff80359d1ae7fd3b5df2
erezlevanon <erez.levanon@mail.huji.ac.il>
2020-06-04 17:04:53 +0300

    Services: Separate service status endpoint to two endpoints.
    Separate the 'services-status' endpoint to two different endpoints,
    one for getting a list of supported services and another for getting
    their status.
    
    Keep old endpoint for debugging purposes.

--- VestibuleServer/urls.py
+++ VestibuleServer/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     url(r'^shows/', include(('shows.urls', 'shows'), namespace='shows')),
     url(r'^torrents/', include(('torrents.urls', 'torrents'), namespace='torrents')),
     url(r'^feeds/', include(('feeds.urls', 'feeds'), namespace='feeds')),
-    url(r'^services-status', views.services_status, name="services-status"),
+    url(r'^services$', views.services, name="services"),
+    url(r'^service-status$', views.service_status, name="service-status"),
+    url(r'^services-status$', views.services_status, name="services-status"),
     url(r'^$', views.Home.as_view(), name='home'),
 ]
