db0ace634b13e9a46a50f914cc4b08b7714e2b70
erezlevanon <erez.levanon@mail.huji.ac.il>
2020-06-16 01:33:58 +0300

    Movies: Arrange admin page for shows.

--- shows/admin.py
+++ shows/admin.py
@@ -3,6 +3,12 @@ from shows.models import Show, ShowProfile
 
 
 class ShowAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': (
+            'type', 'imdb_id', ('title', 'year'), 'status', 'runtime', 'poster_link', 'thumbnail_link', 'profile')}),
+        ("Show Only", {'fields': ('network', 'number_of_seasons', 'next_episode')}),
+        ("Movie Only", {'fields': ('release_date',)}),
+    )
     pass
 
 
