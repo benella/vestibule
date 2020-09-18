01f559d0bf5ffea9ae4fe2d75108decc3e9b586f
Ben <BenElla91@gmail.com>
2020-09-18 17:05:05 +0300

    adding torrents list view

--- torrents/urls.py
+++ torrents/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^download/(?P<torrent_id>\d+)$', views.download_torrent, name='download-torrent'),
+    url(r'^$', views.TorrentList.as_view(), name='list'),
 ]
--- torrents/views.py
+++ torrents/views.py
@@ -1,8 +1,32 @@
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.views import generic
 from .models import Torrent
+from shows.models import Show
 from torrents_manager.transmission_client import TransmissionClient
+
+
+class TorrentList(generic.TemplateView):
+    model = Torrent
+    template_name = "torrent/torrent_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['downloading'] = Torrent.objects.all().filter(
+            download_status=Torrent.DOWNLOADING).order_by("-percent_done")
+
+        context['seeding'] = Torrent.objects.all().filter(
+            download_status=Torrent.READY).order_by("modified")
+
+        found_shows = [show for show in Show.objects.all() if show.last_torrent_found is not None]
+        context['recently_found'] = sorted(found_shows, key=lambda s: s.last_torrent_found, reverse=True)
+
+        active_shows = [show for show in Show.objects.all() if show.last_torrent_activity is not None]
+        context['recent_activity'] = sorted(active_shows, key=lambda s: s.last_torrent_activity, reverse=True)
+
+        return context
 
 
 def download_torrent(request, torrent_id):
